0d348657073b9e2374eb8c704c98afb72d1bceef
aswinashok62 <143902263+aswinashok62@users.noreply.github.com>
2025-02-10 12:43:09 +0530

    settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'admin/templates')], 
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
